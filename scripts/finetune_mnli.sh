738509f7ab2058976f3ac690048df666877ee710
Gabi Stanovsky <gabis@julianm.local>
2019-09-17 13:50:53 -0700

    adding requirements and scripts

--- scripts/finetune_mnli.sh
+++ scripts/finetune_mnli.sh
@@ -4,13 +4,13 @@ export TASK=mnli
 export OUTPUT_PATH=mnli_output
 
 python glue/train.py \
-    --task_name $TASK \
-    --do_train --do_val --do_test --do_val_history \
-    --do_save \
-    --do_lower_case \
-    --bert_model bert-large-uncased \
-    --bert_load_mode from_pretrained \
-    --bert_save_mode model_all \
-    --train_batch_size 24 \
-    --learning_rate 2e-5 \
-    --output_dir $OUTPUT_PATH
+       --task_name $TASK \
+       --do_val --do_test --do_val_history \
+       --do_save \
+       --do_lower_case \
+       --bert_model bert-large-uncased \
+       --bert_load_mode from_pretrained \
+       --bert_save_mode model_all \
+       --train_batch_size 24 \
+       --learning_rate 2e-5 \
+       --output_dir $OUTPUT_PATH
